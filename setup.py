06c275ac2f8bc9bd6082d42ec43370ed19c362cf
Jason Gillman Jr <jgillman@liquidweb.com>
2018-02-16 00:18:29 -0500

    A real simple starter setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cdopy',
+    version='0.1',
+    packages=['cdopy'],
+    url='https://github.com/jgillmanjr/cdopy',
+    license='',
+    author='Jason Gillman Jr.',
+    author_email='',
+    description='A client for working with NOAA\'s Climate Data Online API'
+)
